bf1af64d7e9fe96c46fea8747fe84d72211d7584
zxc8795956 <kobeismvpthebest@gmail.com>
2019-12-02 15:30:39 +0800

    Final version

--- read.py
+++ read.py
@@ -6,6 +6,10 @@ with open('reviews.txt','r') as f:
 		count += 1 #count = count + 1
 		if count % 1000 == 0:
 			print(len(data))
-print(data[0])
-print('-----------')
-print(data[1])
+print('檔案讀取完了，總共有',len(data),'筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+	print(sum_len) 
+print('平均是', sum_len / len(data))
