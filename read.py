c1a837d28af240112d7c8cf97b508e5fd72b996d
zxc8795956 <kobeismvpthebest@gmail.com>
2019-12-02 15:18:31 +0800

    basic read.py

---
+++ read.py
@@ -0,0 +1,11 @@
+data = []
+count = 0
+with open('reviews.txt','r') as f:
+	for line in f:
+		data.append(line)
+		count += 1 #count = count + 1
+		if count % 1000 == 0:
+			print(len(data))
+print(data[0])
+print('-----------')
+print(data[1])
