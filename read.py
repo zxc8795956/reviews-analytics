e500c9812aeae358e6403771ec51e12baa7fb35f
zxc8795956 <kobeismvpthebest@gmail.com>
2019-12-02 15:39:25 +0800

    better version

--- read.py
+++ read.py
@@ -12,4 +12,15 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 	print(sum_len) 
-print('平均是', sum_len / len(data))
+print('留言的平均長度是', sum_len / len(data))
+
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有' , len(new) , '筆留言小於100')
+print(new[0])
+
+
+
